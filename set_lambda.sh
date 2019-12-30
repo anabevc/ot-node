2fa13bbafe24a1825984054b537f6ad7cd386d82
antic11d <dimitrije.misa@gmail.com>
2019-12-30 16:14:54 +0100

    Updated script

--- set_lambda.sh
+++ set_lambda.sh
@@ -8,11 +8,9 @@ else
 fi
 
 if [[ "$1" = "dc" ]]; then
-  sed -i .bckp "s/\(\"dc_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
-  rm "$path.bckp"
+  sed -i "s/\(\"dc_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
 elif [[ "$1" = "dh" ]]; then
-  sed -i .bckp "s/\(\"dh_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
-  rm "$path.bckp"
+  sed -i "s/\(\"dh_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
 fi
 
 #docker restart otnode && docker logs otnode

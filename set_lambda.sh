463b946e660688bd1df0b8eb06b0bafa270ddc53
antic11d <dimitrije.misa@gmail.com>
2019-12-31 13:03:55 +0100

    Handling invalid arguments

--- set_lambda.sh
+++ set_lambda.sh
@@ -6,10 +6,14 @@ else
   path=$3
 fi
 
-if [[ -z $(grep "$1_price_factor" "$path") ]]; then
-  sed -i "/\"blockchain\": {/a \"$1_price_factor\" : \"$2\"," "$path"
+if [[ ("$1" != "dc") && ("$1" != "dh")]]; then
+  echo "Invalid argument 1!"
 else
-  sed -i "s/\(\"$1_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
-fi
+  if [[ -z $(grep "$1_price_factor" "$path") ]]; then
+  sed -i "/\"blockchain\": {/a \"$1_price_factor\" : \"$2\"," "$path"
+  else
+    sed -i "s/\(\"$1_price_factor\" : \)\"[0-9]*\"/\1\"$2\"/g" "$path"
+  fi
 
-docker restart otnode && docker logs otnode -f --tail 1000
+  docker restart otnode && docker logs otnode -f --tail 1000
+fi
